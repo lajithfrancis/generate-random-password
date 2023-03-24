c92c42b7f3ae077bb6b34f71102e61b888540db6
LajithFrancis <lajith.francis@perfomatix.com>
2023-03-24 20:42:05 +0530

    added strict rule

--- src/generate-random-password.js
+++ src/generate-random-password.js
@@ -12,13 +12,36 @@ const { isBoolean } = require('./utils');
  * @param {number} length 
  * @returns 
  */
-const gerenateRandomPassword = (options, length = 10) => {
-    const pool = getPool(options)
+const gerenateRandomPassword = (options, length = 10, strict) => {
+    const { possibleCombinations: pool, opt } = getPool(options)
     let password = '';
     for (let i = 0; i < length; i++) {
         const randomNumber = random(1, pool.length - 1)
         password += pool[randomNumber];
         
+    }
+    if (strict === true && length > 5) {
+        const { lowercase, uppercase, numbers } = getAllCombinations();
+        const destructured = {
+            lowercase: { count: 0, isNeeded: opt.lowercase },
+            uppercase: { count: 0, isNeeded: opt.uppercase },
+            numbers: { count: 0, isNeeded: opt.numbers }
+        }
+        for (let i = 0; i < password.length; i++) {
+            if (lowercase.includes(password[i])) {
+                destructured.lowercase.count += 1;
+            }
+        
+            if (uppercase.includes(password[i])) {
+                destructured.uppercase.count += 1;
+            }
+        
+            if (numbers.includes(password[i])) {
+                destructured.numbers.count += 1;
+            }
+        }
+        if (Object.keys(destructured).some(pass => destructured[pass].count === 0 && destructured[pass].isNeeded === true)) 
+            return gerenateRandomPassword(options, length);
     }
     return password;
 }
@@ -36,7 +59,10 @@ const getPool = (options) => {
     if (!Object.keys(opt).some(type => opt[type]))
         throw new Error('At least one option must be true');
     const possibleCombinations = getPossibleCombinations(opt);
-    return possibleCombinations;
+    return {
+        possibleCombinations,
+        opt
+    };
 }
 
 const getAllCombinations = () => {
@@ -57,6 +83,8 @@ const getPossibleCombinations = (options) => {
     const mappedValues = Object.keys(options).map(type=> options[type] ? allCombinations[type] : '');
     return mappedValues.join('');
 }
+
+console.log('pass: ', gerenateRandomPassword({}, 6))
 
 module.exports = {
     gerenateRandomPassword
